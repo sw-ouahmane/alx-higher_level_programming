adbd21803737739eaf54586cd10a09a532a7be17
abdallah <sw.ouahmane@gmail>
2024-02-02 03:52:34 -0800

    tasks5 corr

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,48 +1,42 @@
-==============================
-How to use 5-text_indentation.py
-==============================
-
-Import file
-    >>> text_indentation = __import__('5-text_indentation').text_indentation
-
-Correct Usage:
-
-    >>> t=" : . ? "
-
-Output for this function should be las this
->>> t="Hello. : \nI am a string? .\n"
+#!/usr/bin/python3
+"""
+text identation module:
+indents text
+useful
+"""
 
 
-t is equal to: 
+def text_indentation(text):
+    """text must be a string, There should be no space
+    at the beginning
+    or at the end of each printed line.
+    """
+    # text is a string, otherwise raise a TypeError
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
 
->>> t=" : . ? "
+    # if text is None or not isinstance(text, str) or len(text) < 0:
+    # raise TypeError('must be a string')
+    # string.replace(old, new, count)
+    # prints a text with 2 new lines
+    text = text.replace('.', '.\n\n')
+    text = text.replace('?', '?\n\n')
+    text = text.replace(':', ':\n\n')
+    # 1) cut the entire line with \n
+    # 2) cut by separator with \n
+    # to the line that was strip (for.,?)
+    # loop text and split where "\n" is)
+    # end =""no space output promp line)
+    # there should be no spaces at the beginning
+    # and the end, line strip metodo
 
+    print("\n".join([line.strip() for line in text.split("\n")]), end="")
 
-On Errors:
-
-empty string: 
->>> text_indentation()
-Traceback (most recent call last):
-TypeError: text_indentation() missing 1 required positional argument: 'text'
-
-more than one string
->>> text_indentation("ble", "ooh", "wah")
-Traceback (most recent call last):
-TypeError: text_indentation() takes 1 positional argument but 3 were given
-
->>> text_indentation(19)
-Traceback (most recent call last):
-TypeError: text must be a string
-
->>> text_indentation("")
-
->>> text_indentation()
-Traceback (most recent call last):
-TypeError: text_indentation() missing 1 required positional argument: 'text'
-
->>> text_indentation("Holberton.")
-Holberton.
-<BLANKLINE>
-
->>> text_indentation("Hello")
-Hello
+    # if isinstance(text, str) is False:
+    # raise TypeError("text must be a string")
+    # delim = ":.?"
+    # for x in delim:
+    # text = text.replace(x, "{}\n\n".format(x))
+    # text = text.replace('\\', '')
+    # text = "\n".join([line.strip() for line in text.split('\n')])
+    # print(text, end="")
