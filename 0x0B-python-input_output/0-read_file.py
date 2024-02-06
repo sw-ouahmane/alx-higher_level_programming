7983758997db6777508339aed917ad6591d67d8d
abdallah <sw.ouahmane@gmail>
2024-02-06 14:17:45 -0800

    First add

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+read_file module
+"""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout"""
+
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
