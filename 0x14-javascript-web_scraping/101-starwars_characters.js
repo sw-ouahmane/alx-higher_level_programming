561fd045ca2fdbcba5c2b66dc19fda00f34f3e43
Abdallah <sw.ouahmane@gmail.com>
2024-05-21 18:48:59 +0100

    Update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
-const urlApi = 'https://swapi.co/api/films/' + process.argv[2];
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request(urlApi, function (error, response, body) {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error); // Print the error if one occurred
-  }
-  const charUrl = JSON.parse(body).characters;
-  console.log(charUrl)
-  for (let i = 0; i < charUrl.length; i++) {
-    await request(charUrl[i], function (error, response, body) {
-      if (error) {
-        console.error(error); // Print the error if one occurred
-      }
-      console.log(JSON.parse(body).name);
-    });
+    console.log(error);
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
   }
 });
